9c78aa8ca50c2904a5538ad8c8284ffc6f9061c2
akurathisrinivas <50867173+akurathisrinivas@users.noreply.github.com>
2023-02-10 22:36:42 +0530

    city cancellation
    city cancellation

--- application/models/admin/City_management_model.php
+++ application/models/admin/City_management_model.php
@@ -143,4 +143,46 @@ class City_management_model extends CI_Model {
 
 
 
+    /* ---------- all_city_cancellation ------------ */
+
+    public function all_city_cancellation($pdata, $getcount = null) {
+        $columns = array
+            (
+            0 => 'first_name',
+            0 => 'number_plate',
+            0 => 'trip_id'
+        );
+        $search_1 = array
+            (
+            1 => 'users.first_name',
+            2 => 'user_vehicles.number_plate',
+            3 => 'rides.trip_id',
+        );
+        if (isset($pdata['search_text_1']) != "") {
+            $this->db->like($search_1[$pdata['search_on_1']], $pdata['search_text_1'], $pdata['search_at_1']);
+        }
+        if ($getcount) {
+            return $this->db->select('rides.id,users.first_name')->join('users', 'users.id=rides.user_id', 'left')->join('user_vehicles', 'user_vehicles.id=rides.vehicle_id')->from('rides')->where('rides.type','taxi')->where('rides.mode','city')->where('rides.status','cancelled')->where('rides.vehicle_type',$pdata['uri_seg'])->order_by('rides.id', 'desc')->get()->num_rows();
+        } else {
+            $this->db->select('rides.*,users.first_name,user_vehicles.number_plate as vehicle_number')->join('users', 'users.id=rides.user_id', 'left')->join('user_vehicles', 'user_vehicles.id=rides.vehicle_id')->from('rides')->where('rides.type','taxi')->where('rides.mode','city')->where('rides.status','cancelled')->where('rides.vehicle_type',$pdata['uri_seg'])->order_by('rides.id', 'desc');
+        }
+        if (isset($pdata['length'])) {
+            $perpage = $pdata['length'];
+            $limit = $pdata['start'];
+            $generatesno = $limit + 1;
+            $orderby_field = $columns[$pdata['order'][0]['column']];
+            $orderby = $pdata['order']['0']['dir'];
+            $this->db->order_by($orderby_field, $orderby);
+            $this->db->limit($perpage, $limit);
+        } else {
+            $generatesno = 0;
+        }
+        $result = $this->db->get()->result_array();
+        foreach ($result as $key => $values) {
+            $result[$key]['sno'] = $generatesno++;
+        }
+        return $result;
+    }
+
+
 }
